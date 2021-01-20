4e6ec977b0ebe04576e498bb216d68a4b8cad482
dbtjdvy2422 <dbtjdvy2422@naver.com>
2021-01-20 23:50:29 +0900

    회원 수정,회원 조회 API

--- jpabook/src/main/java/com/book/project/api/controller/MemberApiController.java
+++ jpabook/src/main/java/com/book/project/api/controller/MemberApiController.java
@@ -1,15 +1,22 @@
 package com.book.project.api.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.project.model.Member;
 import com.book.project.service.MemberService;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +25,33 @@ import lombok.RequiredArgsConstructor;
 public class MemberApiController {
 
 	private final MemberService memberService;
+	
+	
+	@GetMapping("/api/v1/members")
+	public List<Member> membersV1(){
+		return memberService.findMembers();
+	}
+	
+	@GetMapping("/api/v2/members")
+	public Result memberV2() {
+		List<Member> findMembers = memberService.findMembers();
+		List<MemberDto> collect = findMembers.stream()
+			.map(m -> new MemberDto(m.getName()))
+			.collect(Collectors.toList());
+		return new Result(collect);
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class Result<T> {
+		private T data;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class MemberDto {
+		private String name;
+	}
 	
 	@PostMapping("/api/v1/members")
 	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -33,6 +67,29 @@ public class MemberApiController {
 		
 		Long id = memberService.join(member);
 		return new CreateMemberResponse(id);
+	}
+	
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+			@PathVariable("id") Long id,
+			@RequestBody @Valid UpdateMemberRequest request) {
+		
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(),findMember.getName());
+		
+	}
+	
+	@Data
+	static class UpdateMemberRequest {
+		private String name;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
 	}
 	
 	@Data
--- jpabook/src/main/java/com/book/project/controller/MemberController.java
+++ jpabook/src/main/java/com/book/project/controller/MemberController.java
@@ -17,41 +17,41 @@ import com.book.project.web.MemberForm;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
-@RequiredArgsConstructor
-public class MemberController {
-	
- private final MemberService memberService;
- 
- @GetMapping(value = "/members/new")
- public String createForm(Model model) {
- model.addAttribute("memberForm", new MemberForm());
- return "members/createMemberForm";
- }
- 
- 
- @PostMapping(value = "/members/new")
- public String create(@Valid MemberForm form, BindingResult result) {
+	@Controller
+	@RequiredArgsConstructor
+	public class MemberController {
+		
+	 private final MemberService memberService;
 	 
- if (result.hasErrors()) {
- return "members/createMemberForm";
- }
- 
- Address address = new Address(form.getCity(), form.getStreet(),form.getZipcode());
- Member member = new Member(); 
- member.setName(form.getName());
- member.setAddress(address);
- memberService.join(member);
- return "redirect:/";
- }
- 
- //추가
- @GetMapping(value = "/members")
- public String list(Model model) {
- List<Member> members = memberService.findMembers(); 
- model.addAttribute("members", members);
- return "members/memberList";
- }
- 
- 
+	 @GetMapping(value = "/members/new")
+	 public String createForm(Model model) {
+	 model.addAttribute("memberForm", new MemberForm());
+	 return "members/createMemberForm";
+	 }
+	 
+	 
+	 @PostMapping(value = "/members/new")
+	 public String create(@Valid MemberForm form, BindingResult result) {
+		 
+	 if (result.hasErrors()) {
+	 return "members/createMemberForm";
+	 }
+	 
+	 Address address = new Address(form.getCity(), form.getStreet(),form.getZipcode());
+	 Member member = new Member(); 
+	 member.setName(form.getName());
+	 member.setAddress(address);
+	 memberService.join(member);
+	 return "redirect:/";
+	 }
+	 
+	 //추가
+	 @GetMapping(value = "/members")
+	 public String list(Model model) {
+	 List<Member> members = memberService.findMembers(); 
+	 model.addAttribute("members", members);
+	 return "members/memberList";
+	 }
+	 
+	 
 }
